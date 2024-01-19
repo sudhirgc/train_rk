04305d65e140a9aa292814141e9c6c26628d94e0
Sudhir Chandekar <sudhirc@cfi2.com>
2024-01-19 09:45:56 -0500

    smote commit

--- random_us.py
+++ random_us.py
@@ -1,5 +1,5 @@
-from imblearn.under_sampling import RandomUnderSamplerz
-rus = RandomUnderSampler(replacement=True,random_state=42)
+from imblearn.under_sampling import smote
+rus = smote()
 X = df.drop('Class', axis=1).values
 y = df['Class'].values
 X_rus, y_rus = rus.fit_resample(X,y)
